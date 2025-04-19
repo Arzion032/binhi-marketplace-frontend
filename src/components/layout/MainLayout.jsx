f8804c8e9cfe3f77c8570a6ba45d8f29cb8b806f
nisiviloria <nisiashley22@gmail.com>
2025-04-19 23:39:23 +0800

    Fix casing issue in MainLayout path

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,9 +1,10 @@
+// src/components/Layout/MainLayout.jsx
 import React from "react";
 import Header from "../UI/Header";
 
 const MainLayout = ({ children }) => {
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-screen flex flex-col">
       {/* Header */}
       <div className="relative z-30">
         <Header />
@@ -11,12 +12,12 @@ const MainLayout = ({ children }) => {
 
       {/* Background */}
       <div
-        className="absolute inset-0 bg-cover bg-center z-0"
+        className="absolute inset-0 bg-cover bg-center bg-gray-200 z-0"
         style={{ backgroundImage: "url('background.jpg')" }}
       ></div>
 
       {/* Main Content */}
-      <div className="relative z-20">
+      <div className="relative z-20 flex-grow min-h-screen overflow-auto">
         {children}
       </div>
     </div>
