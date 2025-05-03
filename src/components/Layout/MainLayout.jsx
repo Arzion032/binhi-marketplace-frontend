e5873ee81e456f74d86acd42d78e37ec12f91358
Arzion032 <jmelvinsarabia032@gmail.com>
2025-05-03 13:00:43 +0800

    MainLayOut

---
+++ src/components/Layout/MainLayout.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MainHeader from "../UI/MainHeader";
+
+
+const InitialLayout = ({ children }) => {
+  return (
+    <div className="relative min-h-screen">
+      {/* Header */}
+      <div className="relative z-30">
+        <MainHeader />
+      </div>
+
+      {/* Background */}
+      <div
+        className="absolute inset-0 bg-cover bg-center z-20"
+        style={{ backgroundImage: "url('background.jpg')" }}
+      ></div>
+
+      {/* Main Content */}
+      <div className="relative z-20">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default InitialLayout;
