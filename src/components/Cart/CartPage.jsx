ffce79ed8033fbb863e3546799fd4de14a043063
Kaye <kayearroyo42@gmail.com>
2025-05-18 23:34:50 +0800

    Cart Page

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'; 
-import { useNavigate } from 'react-router-dom';
 
-/*Cart Page poo */
+/*Cart Page pooo */
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([
