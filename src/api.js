592d23fc8d3c67725979c6d27a3bd38f92e4bb08
Arzion032 <jmelvinsarabia032@gmail.com>
2025-06-09 01:29:14 +0800

    added backend api link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:8001";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const api = axios.create({
   baseURL: BASE_URL,
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // src/constants.js
-export const BASE_URL = "http://127.0.0.1:8001";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const regions = [
   "Metro Manila",
